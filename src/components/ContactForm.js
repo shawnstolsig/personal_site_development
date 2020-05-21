579e07b02b1abc04801bdd94f226f213bb20c490
Shawn Stolsig <shawnstolsig@gmail.com>
2020-05-20 19:57:38 -0700

    working on emailjs

---
+++ src/components/ContactForm.js
@@ -0,0 +1,17 @@
+// React imports
+import React from 'react'
+
+// Material UI imports
+import {
+    Typography,
+    Grid,
+} from '@material-ui/core'
+
+export default function ContactForm() {
+    // initialize emailjs
+    emailjs.init('user_f0H5XWftXvvGLIre8rF9B');
+
+    return (
+        
+    )
+}
